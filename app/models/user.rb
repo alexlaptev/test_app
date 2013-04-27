f3a138bd2c5d8a25f58c3d5f299f64bdf066a9a7
alexlaptev <hello.kenpark@yandex.ru>
2013-04-27 11:22:10 +0400

    sdasad

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :nickname, :provider, :url, :username
+  attr_accessible :nickname, :provider, :url, :username, :authtoken
 
   def self.find_for_facebook_oauth access_token
     if user = User.where(:url => access_token.info.urls.Facebook).first
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
     if user = User.where(:url => access_token.info.urls.Vkontakte).first
       user
     else 
-      User.create!(:friends => access_token.friends.get, :provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.first_name, :email => access_token.extra.raw_info.screen_name+'@vk.com', :password => Devise.friendly_token[0,20]) 
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.first_name, :email => access_token.extra.raw_info.screen_name+'@vk.com', :password => Devise.friendly_token[0,20]) 
     end
   end
 end
